8219acb235d3881f68fb7754c3839c2cd4ccf49d
sebbycorp <sebastian@maniak.io>
2023-11-21 22:37:36 -0500

    updates

---
+++ s3bucket/output.tf
@@ -0,0 +1,11 @@
+output "bucket" {
+  value = aws_s3_bucket.terraform_state.id
+}
+
+output "key" {
+  value = aws_s3_bucket.terraform_state.id
+}
+
+output "dynamodb_table" {
+  value = aws_dynamodb_table.dynamodb-terraform-state-lock.name
+}
